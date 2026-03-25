1c02fe0865594ce4f4571d89e4fc4b56060f4769
Yannick <yannick@lehr-web.de>
2026-03-25 09:25:49 +0100

    Initial Commit

---
+++ BetterMSFile/BetterMSFileApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterMSFileApp.swift
+//  BetterMSFile
+//
+//  Created by Yannick Aaron Lehr on 25/3/26.
+//
+
+import SwiftUI
+
+@main
+struct BetterMSFileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterMSFile/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BetterMSFile
+//
+//  Created by Yannick Aaron Lehr on 25/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
